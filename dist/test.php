065001e76f48058f5e28c711686c637529282310
txp <tangxp@servyou.com.cn>
2016-12-23 14:02:01 +0800

    增加预览php
    增加预览php

--- dist/test.php
+++ dist/test.php
@@ -2,10 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
 	<title>php-test</title>
 <link href="/webpack_async/dist/css/a.css?3be728290a93403194f6" rel="stylesheet"></head>
 <body>
 	<?php
+		print_r($_SERVER['REMOTE_ADDR']);
 		print_r($_SERVER);
 	?>
 	<script src="./js/min.js"></script>
